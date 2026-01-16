905059a4cb39129fc5316302334c64cef97ea86e
EXA_GAMER_007 <salimovsarvar518@gmail.com>
2026-01-16 16:44:33 +0500

    suuuuu

--- oop.py
+++ oop.py
@@ -1,25 +1,166 @@
 
-class Mashina:
-    def __init__(self,rang,model,tezlik):
-        self.rang = rang
-        self.model = model
-        self.tezlik = tezlik
-car1 = Mashina('Qora','BMW M5','300')
-car2 = Mashina('Qora','BMW i8','250')
-car3 = Mashina('Qora','BMW M4', '250')
+# class Mashina:
+#     def __init__(self,rang,model,tezlik):
+#         self.rang = rang
+#         self.model = model
+#         self.tezlik = tezlik
+# car1 = Mashina('Qora','BMW M5','300')
+# car2 = Mashina('Qora','BMW i8','250')
+# car3 = Mashina('Qora','BMW M4', '250')
 
-print('-'*30)
-print(f'-'*4,"MASHINALAR RO'YXATI",'-'*5)
-print('-'*30)
-print(f"Mashina rangi: {car1.rang}"
-      f"\nMashina modeli: {car1.model}"
-      f"\nMashina tezligi: {car1.tezlik}")
-print('-'*30)
-print(f"Mashina rangi: {car2.rang}"
-      f"\nMashina modeli: {car2.model}"
-      f"\nMashina tezligi: {car2.tezlik}")
-print('-'*30)
-print(f"Mashina rangi: {car3.rang}"
-      f"\nMashina modeli: {car3.model}"
-      f"\nMashina tezligi: {car3.tezlik}")
-print('-'*30)
+# print('-'*30)
+# print(f'-'*4,"MASHINALAR RO'YXATI",'-'*5)
+# print('-'*30)
+# print(f"Mashina rangi: {car1.rang}"
+#       f"\nMashina modeli: {car1.model}"
+#       f"\nMashina tezligi: {car1.tezlik}")
+# print('-'*30)
+# print(f"Mashina rangi: {car2.rang}"
+#       f"\nMashina modeli: {car2.model}"
+#       f"\nMashina tezligi: {car2.tezlik}")
+# print('-'*30)
+# print(f"Mashina rangi: {car3.rang}"
+#       f"\nMashina modeli: {car3.model}"
+#       f"\nMashina tezligi: {car3.tezlik}")
+# print('-'*30)
+#################################################
+#################################################
+#################################################
+##1-masala
+# class Talaba:
+#       def __init__(self,nomi,yoshi,kursi):
+#             self.nomi = nomi
+#             self.yoshi = yoshi
+#             self.kursi = kursi
+# ali = Talaba(f"Ali",20,2)
+# vali = Talaba(f"Vali",21,3)
+# hasan = Talaba(f"Hasan",19,1)
+# husan = Talaba(f"Husan",22,4)
+# olim = Talaba(f"Olim",20,5)
+
+# print('='*30)
+# print(f'='*4,"TALABALAR RO'YXATI",'='*5)
+# print('='*30)
+# print(f"talaba ma'lumotlari: {ali.nomi} {ali.yoshi}-yosh {ali.kursi}-kurs")
+# print(f"talaba ma'lumotlari: {vali.nomi} {vali.yoshi}-yosh {vali.kursi}-kurs")
+# print(f"talaba ma'lumotlari: {hasan.nomi} {hasan.yoshi}-yosh {hasan.kursi}-kurs")
+# print(f"talaba ma'lumotlari: {husan.nomi} {husan.yoshi}-yosh {husan.kursi}-kurs")
+# print(f"talaba ma'lumotlari: {olim.nomi} {olim.yoshi}-yosh {olim.kursi}-kurs")
+# print('='*30)
+#################################################
+#################################################
+#################################################
+# ##2-masala
+# class Kitob:
+#       def __init__(self,nomi,muallifi,narxi):
+#             self.nomi = nomi
+#             self.muallifi = muallifi
+#             self.narxi = narxi
+# kitob1 = Kitob("python dasturlash", "John", 10000)
+# kitob2 = Kitob("Algoritmlar", "Xorazmy", 150000)
+# kitob3 = Kitob("Ma'lumotlar tizimlari", "O'reilly", 200000)
+# kitob4 = Kitob("Python", "Mark Lutz", 250000)
+# kitob5 = Kitob("Java", "Herbert Schildt", 300000)
+# eng_qimmat_kitob = Kitob("Laster kodeksi", "Leonardo davinci", 63000000)
+# print("="*30)
+# print(f"="*4,"KITOB RO'YXATI","="*5)
+# print("="*30)
+# print(f"Kitob ma'lumotlari: Kitob nomi: {kitob1.nomi}\nKitob muallifi: {kitob1.muallifi}\nKitob narxi: {kitob1.narxi}")
+# print("="*30)
+# print(f"Kitob ma'lumotlari: Kitob nomi: {kitob2.nomi}\nKitob muallifi: {kitob2.muallifi}\nKitob narxi: {kitob2.narxi}")
+# print("="*30)
+# print(f"Kitob ma'lumotlari: Kitob nomi: {kitob3.nomi}\nKitob muallifi: {kitob3.muallifi}\nKitob narxi: {kitob3.narxi}")
+# print("="*30)
+# print(f"Kitob ma'lumotlari: Kitob nomi: {kitob4.nomi}\nKitob muallifi: {kitob4.muallifi}\nKitob narxi: {kitob4.narxi}")
+# print("="*30)
+# print(f"Kitob ma'lumotlari: Kitob nomi: {kitob5.nomi}\nKitob muallifi: {kitob5.muallifi}\nKitob narxi: {kitob5.narxi}")
+# print("="*30)
+# print(f"Eng qimmat kitob: Kitob nomi: {eng_qimmat_kitob.nomi}\nKitob muallifi: {eng_qimmat_kitob.muallifi}\nKitob narxi: {eng_qimmat_kitob.narxi}")
+# print("="*30)
+#################################################
+#################################################
+#################################################
+# ##3-masala
+# class Telefon:
+#       def __init__(self,modeli,xotirasi,rangi):
+#             self.modeli = modeli
+#             self.xotirasi = xotirasi
+#             self.rangi = rangi
+# telefon1 = Telefon("iPhone 14", "128GB", "Qora")
+# telefon2 = Telefon("Samsung Galaxy S23", "256GB", "oq")
+# telefon3 = Telefon("Xiaomi 13", "128GB", "Ko'k")
+# telefon4 = Telefon("Huawei P50", "128GB", "Kumush")
+# telefon5 = Telefon("OnePlus 11", "256GB", "yashil")
+# print("="*30)
+# print(f"="*4,"TELEFON RO'YXATI","="*5)
+# print("="*30)
+# print(f"Telefon ma'lumotlari: Telefon modeli: {telefon1.modeli}\nTelefon xotirasi: {telefon1.xotirasi}\nTelefon rangi: {telefon1.rangi}")
+# print("="*30)
+# print(f"Telefon ma'lumotlari: Telefon modeli: {telefon2.modeli}\nTelefon xotirasi: {telefon2.xotirasi}\nTelefon rangi: {telefon2.rangi}")
+# print("="*30)
+# print(f"Telefon ma'lumotlari: Telefon modeli: {telefon3.modeli}\nTelefon xotirasi: {telefon3.xotirasi}\nTelefon rangi: {telefon3.rangi}")
+# print("="*30)
+# print(f"Telefon ma'lumotlari: Telefon modeli: {telefon4.modeli}\nTelefon xotirasi: {telefon4.xotirasi}\nTelefon rangi: {telefon4.rangi}")
+# print("="*30)
+# print(f"Telefon ma'lumotlari: Telefon modeli: {telefon5.modeli}\nTelefon xotirasi: {telefon5.xotirasi}\nTelefon rangi: {telefon5.rangi}")
+# print("="*30)
+##################################################
+##################################################
+##################################################
+# ##4-masala
+# class Meva:
+#       def __init__(self, nomi, vazni, narxi):
+#             self.nomi = nomi
+#             self.vazni = vazni
+#             self.narxi = narxi
+# olma = Meva("Olma", "1kg", 15000)
+# uzum = Meva("Uzum", "0,5kg", 20000)
+# banan = Meva("Banan", "1kg", 12000)
+# anor = Meva("Anor", "1kg", 30000)
+# nok = Meva("Nok", "1kg", 10000)
+# eng_arzon_meva = Meva("Nok", "1kg", 10000)
+# print("="*30)
+# print(f"="*4,"Mevalar ro'yxati","="*5)
+# print("="*30)
+# print(f"Meva ma'lumotlari:\nMeva nomi: {olma.nomi}\nMeva vazni: {olma.vazni}\nMeva narxi: {olma.narxi}")
+# print("="*30)
+# print(f"Meva ma'lumotlari:\nMeva nomi: {uzum.nomi}\nMeva vazni: {uzum.vazni}\nMeva narxi: {uzum.narxi}")
+# print("="*30)
+# print(f"Meva ma'lumotlari:\nMeva nomi: {banan.nomi}\nMeva vazni: {banan.vazni}\nMeva narxi: {banan.narxi}")
+# print("="*30)
+# print(f"Meva ma'lumotlari:\nMeva nomi: {anor.nomi}\nMeva vazni: {anor.vazni}\nMeva narxi: {anor.narxi}")
+# print("="*30)
+# print(f"Meva ma'lumotlari:\nMeva nomi: {nok.nomi}\nMeva vazni: {nok.vazni}\nMeva narxi: {nok.narxi}")
+# print("="*30)
+# print(f"Eng arzon meva:\nMeva nomi: {eng_arzon_meva.nomi},\nnarxi: {eng_arzon_meva.narxi},\nvazni: {eng_arzon_meva.vazni}")
+# print("="*30)
+##################################################
+##################################################
+##################################################
+##5-masala
+class Sportchilar:
+      def __init__(self,nomi,yoshi,sport_turi):
+            self.nomi = nomi
+            self.yoshi = yoshi
+            self.sport_turi = sport_turi
+ali = Sportchilar(f"Ali",25,"Futbol")
+vali = Sportchilar(f"Vali",22,"Basketbol")
+hasan = Sportchilar(f"Hasan",28,"Tennis")
+husan = Sportchilar(f"Husan",20,"Suzish")
+olim = Sportchilar(f"Olim",24,"Voleybol")
+eng_yosh_sportchi = Sportchilar(f"Husan",20,"Suzish")
+print('='*30)
+print(f'='*4,"SPORTCHILAR RO'YXATI",'='*5)
+print('='*30)
+print(f"Sportchi ma'lumotlari:\nNomi: {ali.nomi}\nYoshi: {ali.yoshi}\nSport turi: {ali.sport_turi}")
+print("="*30)
+print(f"Sportchi ma'lumotlari:\nNomi: {vali.nomi}\nYoshi: {vali.yoshi}\nSport turi: {vali.sport_turi}")
+print("="*30)
+print(f"Sportchi ma'lumotlari:\nNomi: {hasan.nomi}\nYoshi: {hasan.yoshi}\nSport turi: {hasan.sport_turi}")
+print("="*30)
+print(f"Sportchi ma'lumotlari:\nNomi: {husan.nomi}\nYoshi: {husan.yoshi}\nSport turi: {husan.sport_turi}")
+print("="*30)
+print(f"Sportchi ma'lumotlari:\nNomi: {olim.nomi}\nYoshi: {olim.yoshi}\nSport turi: {olim.sport_turi}")
+print("="*30)
+print("="*6,"Eng yosh sportchi","="*5,f"\nNomi: {eng_yosh_sportchi.nomi}\nYoshi: {eng_yosh_sportchi.yoshi}\nSport turi: {eng_yosh_sportchi.sport_turi}")
+print("="*30)
